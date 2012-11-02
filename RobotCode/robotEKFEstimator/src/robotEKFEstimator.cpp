2ab6ab89e6df4821e919420233e90b194b2f468d
Parth <apdave@uwaterloo.ca>
2012-11-02 12:06:34 +0100

    Added EKF estimator

---
+++ RobotCode/robotEKFEstimator/src/robotEKFEstimator.cpp
@@ -0,0 +1,8 @@
+#include <ros/ros.h>
+#include <cmath>
+#include <geometry_msgs/Twist.h>
+#include <indoor_pos/ips_msg.h>
+#include <clearpath_horizon/Encoder.h>
+#include <clearpath_horizon/Encoders.h>
+#include <fstream>
+
