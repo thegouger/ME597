21f7f175ff0da4f4e973beaf4370c9fdaa1d10f5
Parth <apdave@uwaterloo.ca>
2012-11-03 17:30:47 +0100

    More EKF code

--- RobotCode/robotEKFEstimator/src/robotEKFEstimator.cpp
+++ RobotCode/robotEKFEstimator/src/robotEKFEstimator.cpp
@@ -11,6 +11,12 @@
 #include <cmath>
 #include <Eigen/Dense>
 
+// globals
+Eigen::Vector3f Y; // measurements
+
+// generate normally distributed random numbers based on box-muller transform
+// or use std::tr1?
+
 using namespace std;
 
 int main()
@@ -25,5 +31,35 @@ int main()
    cout << "M: " << m << endl;
    cout << "M transpose: " << m.transpose() << endl;
 
+   // actual code
+
+   // EKF params
+   Eigen::Vector3f mu_p;
+   Eigen::Vector3f mu; // init with initial measurements
+   Eigen::Matrix   G(3,3);
+   Eigen::Matrix   Sigma_p(3,3);
+   Eigen::Matrix   Sigma(3,3);
+   Eigen::Matrix   Sigma_prev(3,3);
+   Eigen::Matrix   K(3,3);
+   Eigen::Matrix   C(3,3);
+   C = C.Identity();
+
+   // every loop iteration
+   // prediction update
+   mup(1) = mu(1) + v*cos(mu(3))*dt;
+   mup(2) = mu(2) + v*sin(mu(3))*dt;
+   mup(3) = mu(3) + (v*tan(steer)*dt)/L;
+   
+   G(1,1) = 1; G(1,2) = 0; G(1,3) = -v*sin(mu(3));
+   G(2,1) = 0; G(2,2) = 1; G(2,3) = v*cos(mu(3));
+   G(3,1) = 0; G(3,2) = 0; G(3,3) = 1;
+
+   Sigma_p = G*Sigma_prev*(G.transpose()) + R;
+   
+   // measurement update
+   K     = Sigma_p*(C.transpose)*((C*Sigma_p*(C.transpose()) + Q).inverse());
+   mu    = mu_p + K*(Y - C*mu_p);
+   Sigma = (Sigma.identity() - K*C)*Sigma_p;
+   
    return 0;
 }
