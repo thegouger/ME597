b66543d5c01ad94512cece61500a467e15ed1dc8
Parth <apdave@uwaterloo.ca>
2012-11-03 22:12:15 +0100

    Changes to previous EKF file

--- RobotCode/robotEKFEstimator/src/robotEKFEstimator.cpp
+++ RobotCode/robotEKFEstimator/src/robotEKFEstimator.cpp
@@ -21,16 +21,6 @@ using namespace std;
 
 int main()
 {
-   Eigen::MatrixXd m(2,2);
-   
-   m(0,0) = 3;
-   m(1,0) = 2.5;
-   m(0,1) = -1;
-   m(1,1) = 5;
-
-   cout << "M: " << m << endl;
-   cout << "M transpose: " << m.transpose() << endl;
-
    // actual code
 
    // EKF params
