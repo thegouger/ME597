e5c30c8ea9797742c277e209c06ec4c083e00d63
Parth <apdave@uwaterloo.ca>
2012-11-03 22:11:00 +0100

    Added EKF to driving controller

--- RobotCode/robotDrivingController/src/robotDrivingController.cpp
+++ RobotCode/robotDrivingController/src/robotDrivingController.cpp
@@ -1,72 +1,96 @@
+// ros related libraries
 #include <ros/ros.h>
-#include <cmath>
 #include <geometry_msgs/Twist.h>
 #include <indoor_pos/ips_msg.h>
 #include <clearpath_horizon/Encoder.h>
 #include <clearpath_horizon/Encoders.h>
+
+// others
 #include <fstream>
+#include <cmath>
+#include <Eigen/Dense>
+#include <random>
 
 #define ENCODERS 1
 
+using namespace Eigen;
 
 std::ofstream outfile;
 const float MAX_OUTPUT_SPEED = 100.0f;
 
-struct vector2d
-{
-   float x;
-   float y;
-};
+// Measurements
+Vector3f Y;
+const Matrix3f Q( (Matrix3f() << 0.05*0.05, 0,      0,
+                                0,      0.05*0.05,  0,
+                                0,      0           0.0524*0.0524).finished() );
+float current_velocity;
+float steer_angle;
 
-float vector2dMagnitude(vector2d v) { return sqrt(v.x*v.x + v.y*v.y); }
+// EKF params
+Eigen::Vector3f mu;
+Eigen::Vector3f mu_p;
+Eigen::Matrix3f G;
+Eigen::Matrix3f Sigma_p;
+Eigen::Matrix3f Sigma;
+Eigen::Matrix3f K;
+Eigen::Matrix3f C( Matrix3f().Identity() );
 
-// state globals
-vector2d current_pos = {0.0f, 0.0f};
-vector2d old_pos = {0.0f, 0.0f};
-vector2d current_vel = {0.0f, 0.0f};
-vector2d old_vel = {0.0f, 0.0f};
-vector2d delta_vel = {0.0f, 0.0f};
-
-float    current_yaw;
-
-bool     position_acquired; // used to determine whether the IPS has given us a position
-double   old_time;
+bool   position_acquired; // used to determine whether the IPS has given us a position
+double old_time;
 
 void indoorPosCallback(const indoor_pos::ips_msg::ConstPtr& msg)
 {
+   if(!position_acquired) // set mu to initial state
+   {
+      mu(1) = msg->X;
+      mu(2) = msg->Y;
+      mu(3) = msg->Yaw/180.0*3.14159;
+   }
+
    // record time between callbacks to accurately determine velocity
    double timeBetweenCallbacks = ros::Time::now().toSec() - old_time;
    old_time = ros::Time::now().toSec();
 
-   current_pos.x = msg->X;
-   current_pos.y = msg->Y;
-   current_yaw   = msg->Yaw/180.0*3.14159;
+   // Measurements
+   Y(1) = msg->X;
+   Y(2) = msg->Y;
+   Y(3) = msg->Yaw/180.0*3.14159;
 
-   #ifndef ENCODERS
-
-   if(position_acquired)
+   if(!position_acquired)
    {
-      current_vel.x = (current_pos.x - old_pos.x) / timeBetweenCallbacks;
-      current_vel.y = (current_pos.y - old_pos.y) / timeBetweenCallbacks;
-      delta_vel.x = (current_vel.x - old_vel.x) / timeBetweenCallbacks;
-      delta_vel.y = (current_vel.y - old_vel.y) / timeBetweenCallbacks;
+      position_acquired = true;
+      return; // don't proceed with EKF because we don't have a dt yet
    }
 
-   old_pos.x = current_pos.x;
-   old_pos.y = current_pos.y;
-   old_vel.x = current_vel.x;
-   old_vel.y = current_vel.y;
+   Vector3f epsilon;
+   epsilon(1) = std::tr1::normal_distribution<double> normal(0.0f, sqrt(Q(1,1)));
+   epsilon(2) = std::tr1::normal_distribution<double> normal(0.0f, sqrt(Q(2,2,));
+   epsilon(3) = std::tr1::normal_distribution<double> normal(0.0f, sqrt(Q(3,3,));
 
-   #endif
+   Y += epsilon;
 
-   position_acquired = true;
+   // Prediction update
+   mup(1) = mu(1) + v*cos(mu(3))*dt;
+   mup(2) = mu(2) + v*sin(mu(3))*dt;
+   mup(3) = mu(3) + (v*tan(steer)*dt)/L;
+
+   G(1,1) = 1; G(1,2) = 0; G(1,3) = -v*sin(mu(3));
+   G(2,1) = 0; G(2,2) = 1; G(2,3) = v*cos(mu(3));
+   G(3,1) = 0; G(3,2) = 0; G(3,3) = 1;
+
+   Sigma_p = G*Sigma*(G.transpose()) + R;
+
+   // measurement update
+   K     = Sigma_p*(C.transpose)*((C*Sigma_p*(C.transpose()) + Q).inverse());
+   mu    = mu_p + K*(Y - C*mu_p);
+   Sigma = (Matrix3f().identity() - K*C)*Sigma_p;
+
 }
 
 #ifdef ENCODERS
 void encoderCallback(const clearpath_horizon::EncodersConstPtr& msg)
 {
-   current_vel.x = msg->encoders[0].speed;
-   position_acquired = true;
+   current_velocity = msg->encoders[0].speed;
 }
 #endif
 
@@ -78,26 +102,26 @@ int main(int argc, char* argv[])
 
    ros::NodeHandle nodeHandle;
 
-   outfile.open("square_track_PID7_more_tolerance.txt");
+   outfile.open("test.txt");
 
    bool enable_steering = false;  
    bool execute_once = true;
 
    // desired travel velocity
-   double desired_velocity = 0.500; // vel in m/s f
+   double desired_velocity = 0.200; // vel in m/s f
 
    // desired waypoints
-   vector2d waypoints[4]; // 4
-   waypoints[0].x = -1.01f; waypoints[0].y = -.57f;
-   waypoints[1].x = 1.46f;    waypoints[1].y = -.57f;
-   waypoints[2].x = 1.46f;  waypoints[2].y = 1.45f;
-   waypoints[3].x = -1.0f;   waypoints[3].y = 1.45f;
+   Vector2f waypoints[4]; // 4
+   waypoints[0](1) = -1.01f; waypoints[0](2) = -.57f;
+   waypoints[1](1) = 1.46f;  waypoints[1](2) = -.57f;
+   waypoints[2](1) = 1.46f;  waypoints[2](2) = 1.45f;
+   waypoints[3](1) = -1.0f;  waypoints[3](2) = 1.45f;
    int way_state = 1;
 
    outfile << "Desired velocity: " << desired_velocity << "\n";
    outfile << "Waypoints: \n";
    for (int i=0; i<4; i++) {
-      outfile << waypoints[i].x << "," << waypoints[i].y << "\n";
+      outfile << waypoints[i](1) << "," << waypoints[i](2) << "\n";
    }
 
    // PID tuning consts
@@ -105,13 +129,11 @@ int main(int argc, char* argv[])
 
    // Stanley constant
    double ks = 1.0; //  0.5f;
-   // nodeHandle.getParam("stanley_const", ks);
 
    // PID, steering intermediaries
    double err_sum = 0.0f;
 
    double max_steering_angle = 0.394; // 20 deg ?
-   // nodeHandle.getParam("max_steering_angle", max_steering_angle);
 
    // msgs to send/receive
    geometry_msgs::Twist cmd_vel;
@@ -136,74 +158,60 @@ int main(int argc, char* argv[])
       // haven't gotten a callback yet
       if(!position_acquired) // current_pos.x == 0.0)
       {
-	 loop_rate.sleep();
-	 continue;
+	      loop_rate.sleep();
+	      continue;
       }
 
       // PID controller
 
-      vector2d heading_vector = {cos(current_yaw), sin(current_yaw)};
-      float velocity_sign = (heading_vector.x*current_vel.x + heading_vector.y*current_vel.y);
-      velocity_sign = velocity_sign > 0 ? 1.0 : -1.0;
+      Vector2f heading_vector(cos(current_yaw), sin(current_yaw));
 
       #ifdef ENCODERS
-      velocity_sign = current_vel.x > 0 ? 1.0 : -1.0;
+      velocity_sign = current_velocity > 0 ? 1.0 : -1.0;
       ROS_INFO("V sign: %f", velocity_sign);
       #endif
 
       // PID terms
-      float error = desired_velocity - velocity_sign*vector2dMagnitude(current_vel);
+      float error = desired_velocity - velocity_sign*current_velocity;
       err_sum += error;
 
-      float dv = (vector2dMagnitude(current_vel) - vector2dMagnitude(old_vel));
-
-      float vel_output = error * kp + dv * kd + err_sum * ki;
+      float vel_output = error * kp + err_sum * ki;
 
       // limit velocity output
       if(vel_output > MAX_OUTPUT_SPEED)       vel_output = MAX_OUTPUT_SPEED;
-      else if(vel_output < -100) vel_output = -100.0; // don't drive backwards
+      else if(vel_output < -100) vel_output = -100.0;
 
       cmd_vel.linear.x = vel_output;
 
       // Steering controller
-      if(fabs(waypoints[way_state % 4].x - current_pos.x) < 0.30 && fabs(waypoints[way_state % 4].y - current_pos.y) < 0.30f)
+      if( (Vector2f() << waypoints[way_state % 4](1) - mu(1), waypoints[way_state % 4](2) - mu(2)).norm() < 0.3)
       {
          outfile << "Acheived waypoint: " << way_state << "\n";
          ROS_INFO("Acheived waypoint: %d", way_state);
          way_state = way_state % 4 + 1;
       }
 
-      if(sqrt(pow(waypoints[way_state % 4].x - current_pos.x, 2.0) + pow(waypoints[way_state % 4].y - current_pos.y, 2.0) < 0.4))
-      {
-         outfile << "Acheived waypoint: "  << way_state << "\n";
-         ROS_INFO("Acheived waypoint: %d", way_state);
-         way_state = way_state % 4 + 1;
-      }
-
       float x0,y0,x1,y1,x2,y2;
-      x0 = current_pos.x;
-      y0 = current_pos.y;
-      x1 = waypoints[way_state - 1].x;
-      y1 = waypoints[way_state - 1].y;
-      x2 = waypoints[way_state % 4].x;
-      y2 = waypoints[way_state % 4].y;
+      x0 = mu(1);
+      y0 = mu(2);
+      x1 = waypoints[way_state - 1](1);
+      y1 = waypoints[way_state - 1](2);
+      x2 = waypoints[way_state % 4](1);
+      y2 = waypoints[way_state % 4](2);
 
-      vector2d path_vector    = {x2-x1, y2-y1};
-      float heading = acos((path_vector.x * heading_vector.x + path_vector.y * heading_vector.y)/vector2dMagnitude(path_vector)); //atan2((y2-y1), (x2-x1)) - current_yaw;
-      float cross_track_err = ((x2-x1)*(y1-y0) - (x1-x0)*(y2-y1))/sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-      float steer_angle = heading + atan(ks*cross_track_err/vector2dMagnitude(current_vel)); 
-      steer_angle = steer_angle/max_steering_angle * 100.0f;  // percentage
+      Vector2f path_vector(x2-x1, y2-y1);
+      float heading = acos((path_vector.dot(heading_vector))/path_vector.norm());
+      float cross_track_err = ((x2-x1)*(y1-y0) - (x1-x0)*(y2-y1))/path_vector.norm();
+      
+      steer_angle = heading + atan(ks*cross_track_err/current_velocity); // this is a global so it can be accessed by the EKF
+      float steer_angle_normalized = steer_angle/max_steering_angle * 100.0f;  // percentage
 
-      if(steer_angle > 100.0f)       steer_angle = 100.0f;
-      else if(steer_angle < -100.0f) steer_angle = -100.0f;
+      if(steer_angle_normalized > 100.0f) steer_angle_normalized = 100.0f;
+      else if(steer_angle_normalized < -100.0f)      steer_angle_normalized = -100.0f;
 
       ROS_INFO("Current x: %f, Current y: %f, Cross track error: %f, Heading: %f, Steering:%f, Velocity: %f\n", x0, y0, cross_track_err, heading, steer_angle, vel_output);
       
-      outfile<< current_pos.x << " " <<  current_pos.y << " " << vector2dMagnitude(current_vel) << " "<< desired_velocity << " " << vel_output << " " << heading << " " << cross_track_err << " " << steer_angle << "\n";
-
-      // ROS_INFO("Current x: %f, Current y: %f, Error:  %f, Sum: %f \n", x0, y0, error, err_sum);
-
-      cmd_vel.angular.z = steer_angle;
+      cmd_vel.angular.z = steer_angle_normalized;
 
       // publish results
       cmd_vel_pub.publish(cmd_vel);
